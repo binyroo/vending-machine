3f6cfbbcd4a8dcfb9f9c5e30ee175a6617dfaa04
binyroo <binyroo@gmail.com>
2012-04-03 23:04:40 +0900

    DataHelper files created

---
+++ specs/model/DataHelper.qunit.js
@@ -0,0 +1,19 @@
+jQuery(function() {
+
+	module('vm.DataHelper', {
+		setup: function() {
+			
+		},
+
+		teardown: function() {
+			
+		}
+	});
+
+	test('init', function() {
+		
+		var helper = vm.DataHelper;
+
+		ok(helper);
+	});
+});
