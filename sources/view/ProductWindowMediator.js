fbf0ba568377c3af4d43d4d3bfcf370dde5bd72a
binyroo <binyroo@gmail.com>
2012-05-06 23:56:55 +0900

    ProductWindowMediator completed dev and test

---
+++ sources/view/ProductWindowMediator.js
@@ -0,0 +1,59 @@
+
+vm.ProductWindowMediator = iron.Class(puremvc.Mediator, {
+	
+	NAME: 'ProductWindowMediator',
+
+	initialize: function() {
+		
+		puremvc.Mediator.apply(this, [this.NAME, new vm.ProductWindow()]);
+	},
+
+	listNotificationInterests: function() {
+		return [
+			vm.Const.INIT_PRODUCT_LIST,
+			vm.Const.UPDATE_PRODUCT_LIST
+		];
+	},
+
+	handleNotification: function(note) {
+		
+		var comp = this.getViewComponent();
+		
+		switch (note.getName()) {
+
+			case vm.Const.INIT_PRODUCT_LIST:
+
+				var list = note.getBody();
+				comp.setList(list);
+
+				break;
+
+			case vm.Const.UPDATE_PRODUCT_LIST:
+
+				comp.updateOnStock();
+
+				break;
+		}
+	},
+
+	onRegister: function(attribute) {
+		
+		var comp = this.getViewComponent();
+
+		// DOM 엘리먼트 설정
+		comp.setElement($('#product-view ul'));
+		
+		// ViewComponent의 이벤트 수신
+		comp.addListener('select_product', this._onSelectProduct.bind(this));
+		
+		// 상품 선택 시 발생시킬 노티의 커맨드 설정
+		var facade = this.getFacade();
+		facade.registerCommand(vm.Const.SELECT_PRODUCT, vm.SelectProductCommand);
+	},
+
+	_onSelectProduct: function(e) {
+		
+		this.sendNotification(vm.Const.SELECT_PRODUCT, e);
+	}
+
+});
---
+++ specs/view/ProductWindowMediator.qunit.js
@@ -0,0 +1,157 @@
+jQuery(function() {
+	module('ProductWindowMediator', {
+		setup: function() {
+			
+			vm.SelectProductCommand = iron.Class(puremvc.SimpleCommand, {
+				initialize: function() {
+					puremvc.SimpleCommand.apply(this, arguments);
+				},
+
+				execute: function(note) {
+					vm.SelectProductCommand.tempBody = note.getBody();
+				}
+			});
+			vm.SelectProductCommand.tempBody = null;
+
+			vm.ProductWindow = iron.Class(iron.Dispatcher, {
+				initialize: function() {
+					
+				},
+
+				setElement: function(el) {
+					this._el = el;
+				},
+
+				getElement: function() {
+					return this._el;
+				},
+
+				setList: function(list) {
+					
+					this._initialized = true;
+				},
+
+				updateOnStock: function() {
+					this._updatedStock = true;
+				},
+
+				// method for test
+				initialized: function() {
+					return this._initialized;
+				},
+
+				// method for test
+				updated: function() {
+					return this._updatedStock;
+				}
+			});
+
+
+			this.facade = puremvc.Facade.getInstance('TestFacade');
+			
+		},
+
+		teardown: function() {
+			
+			puremvc.Facade.removeCore('TestFacade');
+		}
+	});
+
+	test('Mediator name is ProductWindowMediator', function() {
+
+		var med = new vm.ProductWindowMediator();
+		equal(med.getMediatorName(), 'ProductWindowMediator');
+	});
+
+	test('ViewComponent is ProductWindow', function() {
+		
+		var med = new vm.ProductWindowMediator();
+		ok(med.getViewComponent() instanceof vm.ProductWindow);
+	});
+
+	test('set DOM onRegister', function() {
+		
+		// Given
+		var med = new vm.ProductWindowMediator();
+
+		// When
+		this.facade.registerMediator(med);
+
+		// Then
+		var comp = med.getViewComponent();
+		var el = $(comp.getElement());
+
+		ok(el.is('ul'));
+	});
+
+	test('SelectProductCommand is registed onRegister', function() {
+		
+		// Given
+		var med = new vm.ProductWindowMediator();
+		
+		// When
+		this.facade.registerMediator(med);
+
+		// Then
+		ok(this.facade.hasCommand(vm.Const.SELECT_PRODUCT));
+	});
+
+	test('SelectProductCommand exec', function() {
+		
+		// Given
+		var med = new vm.ProductWindowMediator();
+		this.facade.registerMediator(med);
+
+		// When
+		var comp = med.getViewComponent();
+		var selectedIndex = -1;
+
+		comp.dispatch('select_product', { selectedIndex: 0 });
+
+		// Then
+		selectedIndex = vm.SelectProductCommand.tempBody.selectedIndex;
+		equal(selectedIndex, 0);
+	});
+
+	test('listNotificationInterests', function() {
+		
+		// Given
+		var med = new vm.ProductWindowMediator();
+
+		// When
+		// Then
+		deepEqual(med.listNotificationInterests(), [
+			vm.Const.INIT_PRODUCT_LIST,
+			vm.Const.UPDATE_PRODUCT_LIST
+		]);
+	});
+
+	test('handle notification INIT_PRODUCT_LIST', function() {
+		
+		// Given
+		var med = new vm.ProductWindowMediator();
+		this.facade.registerMediator(med);
+
+		// When
+		this.facade.sendNotification(vm.Const.INIT_PRODUCT_LIST, {});
+
+		// Then
+		var comp = med.getViewComponent();
+		ok(comp.initialized());
+	});
+
+	test('handle notification UPDATE_PRODUCT_LIST', function() {
+		
+		// Given
+		var med = new vm.ProductWindowMediator();
+		this.facade.registerMediator(med);
+		this.facade.sendNotification(vm.Const.INIT_PRODUCT_LIST, {});
+
+		// When
+		this.facade.sendNotification(vm.Const.UPDATE_PRODUCT_LIST, { insertedMoney: 9000 });
+
+		// Then
+		var comp = med.getViewComponent();
+		ok(comp.updated());
+	});
+});
