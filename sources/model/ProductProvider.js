a3f58a916518ed031c714c100b1710b808570bd9
binyroo <binyroo@gmail.com>
2012-05-07 00:13:36 +0900

    Image's base path changed

--- sources/model/ProductProvider.js
+++ sources/model/ProductProvider.js
@@ -6,7 +6,7 @@ var Const = {
 
 	PRODUCT_NAME: ["팹시", "V10", "녹차", "맹물", "환타", "식혜", "국물", "커피"],
 
-	IMAGE_PREFIX: 'http://binyroo.github.com/VendingMachine/img/pImg',
+	IMAGE_PREFIX: 'img/pImg',
 	IMAGE_EXTENSION: '.png',
 	IMAGE_ID_MAX: 22,
 	IMAGE_ID_MIN: 0,
