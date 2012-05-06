b3e86d7683ee0152a1ec4864dea2d0abc1c4a8f6
binyroo <binyroo@gmail.com>
2012-05-07 00:04:06 +0900

    Notification Name is added

--- sources/common/Common.js
+++ sources/common/Common.js
@@ -1,2 +1,9 @@
 if (!window.binyroo) binyroo = {};
 if (!window.vm) vm = {};
+
+vm.Const = {
+
+	INIT_PRODUCT_LIST: 'initProductList',
+	UPDATE_PRODUCT_LIST: 'updateProductList',
+	SELECT_PRODUCT: 'selectProduct'
+};
